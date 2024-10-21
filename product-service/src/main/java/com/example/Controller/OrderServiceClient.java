ec722f74db793ef32ef2ad70c7b4706e83788c33
MoemenAshraf <moemenashraf74@yahoo.com>
2024-10-21 13:13:16 +0300

    Gateway Api

--- product-service/src/main/java/com/example/Controller/OrderServiceClient.java
+++ product-service/src/main/java/com/example/Controller/OrderServiceClient.java
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import java.util.List;
 
-@FeignClient(name = "order-service")
+@FeignClient(name = "order-service",url = "http://localhost:8080")
 public interface OrderServiceClient {
 
     @GetMapping("/orders")
     String getOrderById();
+
 }
